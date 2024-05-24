3bb038d6c27336f9365a11aeccbfce623c9b00b5
doflado <kusankein111@gmail.com>
2024-05-23 23:03:12 -0400

    update eth token balance api

---
+++ src/controller/sol_controller.ts
@@ -0,0 +1,51 @@
+import { Alchemy, Network, TokenBalance } from "alchemy-sdk";
+import dotConfig from "dotenv";
+
+dotConfig.config();
+const apiKey = process.env.ALCHEMY_API_KEY;
+
+const config = {
+  apiKey,
+  network: Network.ETH_MAINNET,
+};
+
+// initalize alchemy instance for alchemy eth
+const alchemy = new Alchemy(config);
+
+// parameter
+// @owner address to fetch tokens lists on-hand
+export const getTokenList = async (owner: string) => {
+  // Get token balances
+  const balances = await alchemy.core.getTokenBalances(owner);
+  console.log(balances);
+  // Remove balances with zero balance
+  const nonZeroBalances = balances.tokenBalances.filter(
+    (item: TokenBalance) => item.tokenBalance !== "0"
+  );
+
+  // initalize list for tokens on owner address.
+  const tokenList = [];
+  let i = 0;
+
+  // loop through all tokens in non-zero Tokens
+  for (i = 0; i < nonZeroBalances.length; i++) {
+    const token: TokenBalance = nonZeroBalances[i];
+    // Get balance for current token in non-zero balance list.
+    let balance = parseFloat(token.tokenBalance as string);
+
+    // Get meta data for current token
+    const metaData = await alchemy.core.getTokenMetadata(token.contractAddress);
+
+    // Compute token balance in user friendly format
+    // balance /= Math.pow(10, metaData.decimals as number);
+    balance = parseFloat(balance.toFixed(3));
+
+    tokenList.push({
+      ...metaData,
+      balance: token.tokenBalance,
+      address: token.contractAddress,
+    });
+  }
+
+  return tokenList;
+};
