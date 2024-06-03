322763ec1c8cd6ea84bb9c2acd4eb5b476986ec6
doflado <kyotomo920@gmail.com>
2024-06-03 08:47:49 +0000

    update apis

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,7 @@ import { runServer } from "./const";
 dotConfig.config();
 
 const app: Express = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5001;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
