1031fe69441c1982329f97a99c5e1b7ae7ed3410
whalexlord <hookcaptain409@gmail.com>
2024-06-04 13:51:41 -0400

    complete converting to js

--- const.js
+++ const.js
@@ -1,7 +1,5 @@
-import Moralis from "moralis";
-import dotConfig from "dotenv";
-
-dotConfig.config();
+const Moralis = require("moralis").default;
+require("dotenv").config();
 
 const apiKey = process.env.MORALIS_API_KEY;
 
@@ -9,7 +7,11 @@ const config = {
   apiKey,
 };
 
-export const runServer = async () => {
+const runServer = async () => {
     console.log("Running moralis server");
     Moralis.start(config);
+}
+
+module.exports = {
+  runServer
 }
